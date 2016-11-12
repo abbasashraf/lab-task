c368587335a7aa84ad3fbb4073691be4d1d78a7a
Hamza <Hamzanasim_cs@yahoo.com>
2016-11-12 16:09:50 +0500

    asdasd

---
+++ app.js
@@ -0,0 +1,105 @@
+
+
+
+// var num = 45 +5 -4 * (10 /1) % 1 ;
+// alert(num);
+
+
+
+// function name(){
+
+// var num = 27.2727;
+// var num1 = Math.floor(num);
+// alert(num1);
+
+// }
+// name();
+
+
+// function new1(){
+    
+//    var random = Math.random();
+//    random1 = random * 10 + 10 ;
+//    random3 = Math.floor(random1);
+
+// alert(random3);
+// }
+
+// new1();
+
+
+// ++a + a -a --a ;
+
+
+// function task3(){
+// var name = "Helloworld";
+
+// var newname = name.slice(5,name.length);
+// alert(newname);
+// }
+
+// task3();
+
+
+// function task4(){
+// var name = "Helloghjghjghjworld";
+// var iname = name.indexOf("w");
+// var lname = name.indexOf("d");
+// var newname = name.slice(iname,lname + 1);
+
+// alert(newname);
+
+// var length = newname.length;
+// alert(length);
+// }
+
+// task4();
+
+
+
+// var a = 10 ;
+
+// if(true){
+
+// console.log("IF");
+
+// }
+// else if(true){
+// console.log( " ELSE IF");
+
+// }
+// else{
+// console.log("ELSE");
+// }
+
+
+// var cities = [["karachi","sakhar","badin"],["punjab","lahore","islamabad"]];
+
+// for(var i = 0; i < cities.length; i++){
+//     for(var j = 0 ; j < cities[i].length; j++){
+//         alert(cities[i][j]);
+//     }
+// }
+
+
+
+
+
+
+// var wname = "helloworld";
+
+
+// for(var i = 0; i< wname.length; i++){
+//     var ind = wname.indexOf(i);
+//     alert(ind);
+// }
+
+var weekday= "sun";
+switch(weekday){
+    case "sun":
+    alert("sunday");
+    case 
+
+
+
+}
